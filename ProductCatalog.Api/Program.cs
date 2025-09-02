e8fcc130349a8bf97522024bbaa78dff836cb79c
Tunde Ogundiji <tundeogundiji@gmail.com>
2025-09-02 18:23:08 +0100

    add migration &  persistence service configuration

--- ProductCatalog.Api/Program.cs
+++ ProductCatalog.Api/Program.cs
@@ -1,4 +1,8 @@
+using ProductCatalog.Persistence;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigurePersistenceServices(builder.Configuration);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
---
+++ ProductCatalog.Persistence/PersistenceServiceConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace ProductCatalog.Persistence
+{
+    public static class PersistenceServiceConfiguration
+    {
+        public static IServiceCollection ConfigurePersistenceServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddSqlServer<ProductCatalogDbContext>(configuration.GetConnectionString("DefaultConnection"));
+            return services;
+
+        }
+    }
+}
